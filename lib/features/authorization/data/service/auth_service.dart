db15b39feb46b4b44abf3076dc879a572dd2cbd7
Persifal <md.putilin@yandex.ru>
2024-06-27 03:08:56 +0300

    P24SA-6 Create auth service

---
+++ lib/features/authorization/data/service/auth_service.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:allservice/features/authorization/domain/user_login_request.dart';
+import 'package:allservice/token/domain/jwt_response.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+part 'auth_service.g.dart';
+
+@RestApi()
+abstract class AuthService {
+  factory AuthService(
+    Dio dio
+  ) = _AuthService;
+
+  @POST('/users/login/')
+  Future<JwtResponse> loginUser({
+    @Body() required UserLoginRequest request,
+  });
+}
