b0895a02ecb4f9120c5bc811e68456d54b0dbe52
Persifal <md.putilin@yandex.ru>
2024-06-30 17:31:10 +0300

    P24SA-60 Add registration repository

--- lib/features/registration/data/repository/registration_repository.dart
+++ lib/features/registration/data/repository/registration_repository.dart
@@ -1,19 +1,19 @@
+import 'package:allservice/features/recover_password/domain/message_response/message_response.dart';
 import 'package:allservice/features/registration/data/service/registration_service.dart';
 import 'package:allservice/features/registration/domain/user_registration_request.dart';
-import 'package:allservice/token/domain/jwt_response.dart';
 
 class RegistrationRepository {
-  final RegistrationService _registrarionService;
+  final RegistrationService _registrationService;
   
   RegistrationRepository(
-    this._registrarionService,
+    this._registrationService,
   );
 
-  Future<JwtResponse> register({
+  Future<MessageResponse> register({
     required UserRegistrationRequest request,
   }) async {
     try {
-      final result = await _registrarionService.registerUser(
+      final result = await _registrationService.registerUser(
         request: request,
       );
       return result;
