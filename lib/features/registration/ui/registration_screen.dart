c1cde44ca686c6f5aad2fc47c5daa7b29e43ba85
Persifal <md.putilin@yandex.ru>
2024-06-30 17:22:32 +0300

    P24SA-69 Refactoring registration screen

--- lib/features/registration/ui/registration_screen.dart
+++ lib/features/registration/ui/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:allservice/features/registration/provider/registration_provider.dart';
 import 'package:allservice/res/constants/constants.dart';
+import 'package:allservice/res/icons/all_service_icons.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,6 +17,12 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   bool _passwordVisible = true;
 
   @override
+  void initState() {
+    super.initState();
+    Provider.of<RegistrationScreenProvider>(context, listen: false).reset();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final provider = Provider.of<RegistrationScreenProvider>(context);
 
@@ -24,18 +31,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         key: provider.formKey,
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 190),
+                const SizedBox(height: 150),
                 Text('Регистрация', style: titleTextStyle),
-                const SizedBox(height: 60),
+                const SizedBox(height: 35),
                 (provider.isLoading)
                       ? const Align(
                           alignment: Alignment.bottomCenter,
                           child: SizedBox(
-                            height: 190,
+                            height: 370,
                             child: Center(child: CircularProgressIndicator())
                           )
                         )
@@ -47,7 +54,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                                 controller: provider.firstNameController,
                                 decoration: const InputDecoration(
                                   labelText: 'Имя',
-                                  //prefixIcon:  Icon(AllServiceIcons.email, size: 20, color: kIconColor),
+                                  prefixIcon:  Icon(AllServiceIcons.profile, size: 20, color: kIconColor),
                                 ),
                                 validator:(value) => provider.nameValidator(value),
                               ),
@@ -58,7 +65,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                                 controller: provider.secondNameController,
                                 decoration: const InputDecoration(
                                   labelText: 'Фамилия',
-                                  //prefixIcon:  Icon(AllServiceIcons.email, size: 20, color: kIconColor),
+                                  prefixIcon:  Icon(AllServiceIcons.profile, size: 20, color: kIconColor),
                                 ),
                                 validator:(value) => provider.nameValidator(value),
                               ),
@@ -69,7 +76,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                                 controller: provider.emailController,
                                 decoration: const InputDecoration(
                                   labelText: 'Электронная почта',
-                                  //prefixIcon:  Icon(AllServiceIcons.email, size: 20, color: kIconColor),
+                                  prefixIcon:  Icon(AllServiceIcons.email, size: 20, color: kIconColor),
                                 ),
                                 validator:(value) => provider.emailValidator(value),
                               ),
@@ -81,21 +88,74 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                                 obscureText: _passwordVisible,
                                 decoration: InputDecoration(
                                   labelText: 'Пароль',
-                                 // prefixIcon: const Icon(AllServiceIcons.lock, size: 20, color: kIconColor),
+                                 prefixIcon: const Icon(AllServiceIcons.lock, size: 20, color: kIconColor),
                                   suffixIcon: _buildSuffixIcon(),
                                 ),
                                 validator: (value) => provider.passwordValidator(value),
                               ),
                             ),
+                            GestureDetector(
+                              child: Row(
+                                children: [
+                                  Checkbox(
+                                    checkColor: provider.checkColor,
+                                    activeColor: Colors.white,
+                                    hoverColor: provider.checkColor,
+                                    value: provider.isCheck,
+                                    side: BorderSide(
+                                      color: provider.checkColor,
+                                      width: 1.5
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4.0),
+                                    ),
+                                    onChanged: (bool? bool) {
+                                      provider.changeIsCheck();
+                                    }
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                    child: Text(
+                                      'Я принимаю пользовательское \nсоглашение и политику конфиденциальности',
+                                      style: inputFieldTextStyle.copyWith(color: provider.checkTextColor),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              onTap: () {
+                                provider.changeIsCheck();
+                              },
+                            )
                           ],
                         ),
-                const SizedBox(height: 30),
-                ElevatedButton(
-                  child: const Text('Зарегистрироваться'),
-                  onPressed: () {
-                    provider.register(context);
-                  },
-                ),
+                const SizedBox(height: 35),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      child: const Text('Зарегистрироваться'),
+                      onPressed: () {
+                        provider.register(context);
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Уже есть аккаунт? ', style: inputFieldTextStyle),
+                        GestureDetector(
+                          child: Text(
+                            'Войти', 
+                            style: inputFieldTextStyle.copyWith(color: kPrimaryColor)
+                          ),
+                          onTap: () {
+                            provider.navigateToAuthorization(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                )
               ],
             ),
           ),
